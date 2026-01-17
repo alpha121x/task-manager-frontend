d1c63fd4294d4eb1ce0d40ba142e646962acdfd3
Abbasch <109143082+alpha121x@users.noreply.github.com>
2026-01-17 19:59:06 +0500

    Update app.vue

--- src/app.vue
+++ src/app.vue
@@ -1,10 +1,9 @@
-<!-- src/app.vue -->
 <template>
   <div>
     <NuxtPage />
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang=""ts"">
 // This enables automatic routing to /login, /dashboard, etc.
 </script>
